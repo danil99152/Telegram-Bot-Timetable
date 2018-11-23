525902cbcf7ce885167741cd15b13589e6f83650
Danil Komyshev <danil.komyshev777@gmail.com>
2018-11-24 01:13:24 +0400

    Работаем!

---
+++ TimetableBot/TimetableBot/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace TimetableBot.Commands
+{
+    public abstract class Command
+    {
+        public abstract string Name { get; }
+        public abstract void Execute(Message message, TelegramBotClient client);
+        public bool Contains(string command)
+        {
+            return command.Contains(this.Name);
+        }
+    }
+}
---
+++ TimetableBot/TimetableBot/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimetableBot.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Привет, УдГУ!";
+        }
+
+    }
+}
