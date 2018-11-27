11e0da7f2b657b71357743ca4586961465223a4f
Danil Komyshev <danil.komyshev777@gmail.com>
2018-11-27 18:57:38 +0400

    123

---
+++ Bot v.2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Telegram.Bot;
+using System.Net;
+using Telegram.Bot.Args;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static ITelegramBotClient botClient;
+        static void Main()
+        {
+            var httpProxy = new WebProxy(Address: "HSI-KBW-109-192-091-198.hsi6.kabel-badenwuerttemberg.de:53701") { };
+             botClient = new TelegramBotClient("604930561:AAEs7iq0mmr0h9eQA7dBxoRo7kjGpyiW9eY", httpProxy);
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                  chatId: e.Message.Chat,
+                  text: "You said:\n" + e.Message.Text
+                );
+            }
+        }
+    }
+}
+
