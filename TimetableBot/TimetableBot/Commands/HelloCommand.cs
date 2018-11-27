7d14cfe9e2580c0b490892c67063af9b6cca244b
Danil Komyshev <danil.komyshev777@gmail.com>
2018-11-27 20:41:20 +0400

    Теперь консоль

---
+++ TimetableBot/TimetableBot/Commands/HelloCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TimetableBot.Models.Commands
+{
+    public class HelloCommand : Command
+    {
+        public override string Name => "hello";
+
+        public override async void Execute(Message message, TelegramBotClient client)
+        {
+            string[] replayes = { "Hello!", "СапБой", "Я в УдГУ всё могу!"};
+            var chatId = message.Chat.Id;
+            var messageId = message.MessageId;
+
+            //логика бота
+
+            await client.SendTextMessageAsync(chatId, replayes[new Random().Next(0, replayes.Length)]);
+        }
+    }
+}
---
+++ TimetableBot/TimetableBot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Telegram.Bot;
+using System.Net;
+using Telegram.Bot.Args;
+using System.Threading;
+
+namespace TimetableBot
+{
+    class Program
+    {
+        static ITelegramBotClient botClient;
+        static void Main()
+        {
+            var httpProxy = new WebProxy(Address: "HSI-KBW-109-192-091-198.hsi6.kabel-badenwuerttemberg.de:53701") { };
+            botClient = new TelegramBotClient("715240208:AAHS-H8AUdR2Wr4eOm4wdJKPWNle7CI4V1E", httpProxy);
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                  chatId: e.Message.Chat,
+                  text: "You said:\n" + e.Message.Text
+                );
+            }
+        }
+    }
+}
+
