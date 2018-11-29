b1f64bdab08ddd55949c335a76470ffeeaec3123
Danil Komyshev <danil.komyshev777@gmail.com>
2018-11-29 18:48:43 +0400

    БД

---
+++ TimetableBot/TimetableBot/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimetableBot
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FIO { get; set; }
+        public string Phone { get; set; }
+    }
+}
